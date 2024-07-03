6dda13bf48f9b12a9d73a5d47dbea4c7ac90c39b
Shreyas Kousik <shreyas.kousik@gmail.com>
2024-07-03 15:50:12 -0400

    added script to run just one planning iteration
    this calls P.replan instead of running the simulator

--- simulation/run_quadrotor_simulation_one_iteration.m
+++ simulation/run_quadrotor_simulation_one_iteration.m
@@ -15,33 +15,20 @@ tbl_filename = 'quadrotor_tracking_error_table_dt0.02_vmax_5.25_zonotope.mat' ;
 % world
 bounds = [0 80 -10 10 0 10] ;
 goal_radius = 1.5 ;
-N_tall = 20 ;
-N_wide = 20 ;
-N_long = 0 ;
-N_boxy = 20 ;
+N_tall = 20 ; % number of "tall" obstacles
+N_wide = 20 ; % number of "wide" obstacles
+N_long = 0 ; % number of "long" obstacles
+N_boxy = 20 ; % number of "boxy" obstacles
 tall_dims = [0.5 1] ;
 wide_dims = [0.25 0.75] ;
 boxy_dims = [1 7 3 1] ;
-obs_color = [1 0.7 0.7] ;
-obs_opacity = 0.5 ;
-
-% simulator
-allow_replan_errors = true ;
-verbose_level = 10 ;
-max_sim_time = 1e6 ;
-max_sim_iter = 50 ;
 
 % plotting
-line_width = 2 ;
-plot_while_running = true ;
-animate_after_run = false ;
-plot_planning_times = false ;
 agent_camera_distance = 3 ; % default is 3
 agent_camera_position = [-3;0;1] ; % default is [-3;0;1.5]
 agent_camera_view = 'behind' ; % none, behind, above, or onboard
 plot_zono_FRS_flag = true ;
 plot_zono_style = 'tube' ; % 'face' or 'tube'
-save_gif = false ; % note that this will run really slowly at hi-res
 
 %% automated from here
 % load FRS
@@ -84,61 +71,25 @@ P = quadrotor_zono_RTD_planner(FRS,tbl,'t_move',0.5,'verbose',verbose_level,...
     'plot_zonotope_reach_set_flag',plot_zono_FRS_flag,...
     'plot_zono_style',plot_zono_style) ;
 
-% set up simulator
-S = simulator(A,W,P,'verbose',verbose_level,...
-    'allow_replan_errors',allow_replan_errors,...
-    'max_sim_time',max_sim_time,'max_sim_iterations',max_sim_iter,...
-    'plot_while_running',plot_while_running) ;
-
 %% let it rip
-summary = S.run() ;
+% get agent and world ready
+W.reset() ;
+A.reset(W.start) ; 
 
-%% stop agent at end
-stop(A)
+% get planner ready
+agent_info = A.get_agent_info() ;
+world_info = W.get_world_info(agent_info,P) ;
+P.setup(agent_info,world_info) ;
 
-%% set linewidths
-A.plot_data.body.LineWidth = line_width ;
-A.plot_data.sides.LineWidth = line_width ;
-A.plot_data.rotor.LineWidth = line_width ;
-A.plot_data.reference_trajectory.LineWidth = line_width ;
-A.plot_data.trajectory.LineWidth = line_width ;
+% call planner
+[T_nom,U_nom,Z_nom] = P.replan(agent_info,world_info) ;
+A.move(P.t_move,T_nom,U_nom,Z_nom) ;
 
-for idx = 1:W.N_obstacles
-    o = W.obstacles{idx} ;
-    o.plot_data.body.LineWidth = line_width ;
-end
+%% plotting
+figure(1) ; cla ; hold on ; axis equal ; axis(W.bounds)
 
-%% planning times
-if plot_planning_times
-    figure(2) ; clf ; hold on ;
-    plot(summary.planning_time)
-    N = length(summary.planning_time) ;
-    plot(summary.total_iterations,[P.t_plan, P.t_plan],'r--')
-    xlabel('planning iterations')
-    ylabel('planning time [s]')
-    set(gca,'FontSize',15)
-    ylim([0,P.t_plan+0.1])
-end
+plot(W)
+plot(A)
+plot(P)
 
-%% report speed
-[v_stats,a_stats] = A.report_speed_and_accel_stats ;
-
-disp(['Speed min/mean/max: ',num2str(v_stats,'%0.2f ')])
-disp(['Accel min/mean/max: ',num2str(a_stats,'%0.2f ')])
-
-%% animation
-if animate_after_run
-    figure(1)
-    clf
-    hold on
-    plot(W)
-    axis equal
-    plot_at_time(A)
-    camlight
-    lighting flat
-    material dull
-    lightangle(10,70)
-    
-    %% animate
-    animate(S,1,1,save_gif)
-end
+set_plot_linewidths(1.5)
